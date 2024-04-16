6c3ed13eb57f62374655169f6988a7bdd4cb60b3
Martin Procházka <juniwalk@outlook.cz>
2024-04-16 14:08:25 +0200

    Added new FTP service

---
+++ src/FTPService.php
@@ -0,0 +1,106 @@
+<?php declare(strict_types=1);
+
+/**
+ * @copyright Martin Procházka (c) 2024
+ * @license   MIT License
+ */
+
+namespace JuniWalk\SSH;
+
+use FTP\Connection;
+use JuniWalk\SSH\Authentications\None;
+use JuniWalk\SSH\Exceptions\AuthenticationException;
+use JuniWalk\SSH\Exceptions\ConnectionException;
+use JuniWalk\SSH\Subsystems;
+
+final class FTPService implements Service
+{
+	use Subsystems\FTP;
+
+	private Connection $session;
+
+	public function __construct(
+		private ?string $host = null,
+		private int $port = 21,
+		private Authentication $auth = new None('anonymous'),
+	) {
+		if ($host === null) {
+			return;
+		}
+
+		$this->connect($host, $port, $auth);
+	}
+
+
+	public function __destruct()
+	{
+		$this->disconnect();
+	}
+
+
+	public function getHost(): ?string
+	{
+		return $this->host;
+	}
+
+
+	public function getPort(): int
+	{
+		return $this->port;
+	}
+
+
+	public function setPassive(bool $passive = true): bool
+	{
+		return ftp_pasv($this->session, $passive);
+	}
+
+
+	public function isConnected(): bool
+	{
+		return $this->session instanceof Connection;
+	}
+
+
+	/**
+	 * @throws AuthenticationException
+	 * @throws ConnectionException
+	 */
+	public function connect(string $host, int $port = 21, Authentication $auth = null): static
+	{
+		$this->isConnected() && $this->disconnect();
+
+		$auth ??= $this->auth;
+		$session = match (true) {
+			str_starts_with($host, 'ftps://')	=> ftp_ssl_connect($host, $port),
+			str_starts_with($host, 'ftp://')	=> ftp_connect($host, $port),
+
+			default => throw ConnectionException::fromProtocol($host.':'.$port, ['ftp', 'ftps']),
+		};
+
+		if (!$session instanceof Connection) {
+			throw ConnectionException::fromLastError($host.':'.$port);
+		}
+
+		if (!$auth || !$auth->login($session)) {
+			throw AuthenticationException::fromAuth($auth);
+		}
+
+		$this->session = $session;
+		$this->auth = $auth;
+		$this->host = $host;
+		$this->port = $port;
+
+		return $this;
+	}
+
+
+	public function disconnect(): void
+	{
+		if ($this->isConnected()) {
+			ftp_close($this->session);
+		}
+
+		$this->session = null;
+	}
+}
---
+++ src/Subsystems/FTP.php
@@ -0,0 +1,111 @@
+<?php declare(strict_types=1);
+
+/**
+ * @copyright Martin Procházka (c) 2024
+ * @license   MIT License
+ */
+
+namespace JuniWalk\SSH\Subsystems;
+
+use JuniWalk\SSH\Authentications\Password;
+use JuniWalk\SSH\Exceptions\FileHandlingException;
+use Nette\Http\Url;
+
+trait FTP
+{
+	/**
+	 * @throws FileHandlingException
+	 */
+	public function write(string $remoteFile, string $content, int $mode = 0644): bool
+	{
+		$stream = fopen('php://temp', 'r+');
+		fwrite($stream, $content);
+		rewind($stream);
+
+		if (!ftp_fput($this->session, $remoteFile, $stream)) {
+			throw FileHandlingException::fromFile($remoteFile, 'Could not write data to file');
+		}
+
+		fclose($stream);
+
+		return $this->chmod($remoteFile, $mode);
+	}
+
+
+	/**
+	 * @throws FileHandlingException
+	 */
+	public function read(string $remoteFile): string
+	{
+		$stream = fopen('php://temp', 'r+');
+
+		if (!ftp_fget($this->session, $stream, $remoteFile)) {
+			throw FileHandlingException::fromFile($remoteFile, 'Could not write data to file');
+		}
+
+		$contents = stream_get_contents($stream);
+		fclose($stream);
+
+		if ($contents === false) {
+			throw FileHandlingException::fromFile($remoteFile, 'Unable to read remote file');
+		}
+
+		return $contents;
+	}
+
+
+	/**
+	 * @throws FileHandlingException
+	 */
+	public function send(string $localFile, string $remoteFile, int $mode = 0644): bool
+	{
+		$content = @file_get_contents($localFile);
+
+		if ($content === false) {
+			throw FileHandlingException::fromFile($localFile, 'Could not open local file');
+		}
+
+		return $this->write($remoteFile, $content, $mode);
+	}
+
+
+	/**
+	 * @throws FileHandlingException
+	 */
+	public function receive(string $remoteFile, string $localFile, bool $overwrite = true): bool
+	{
+		if (!$overwrite && is_file($localFile)) {
+			throw FileHandlingException::fromFile($localFile, 'Local file already exists');
+		}
+
+		return file_put_contents($localFile, $this->read($remoteFile)) !== false;
+	}
+
+
+	public function unlink(string $remoteFile): bool
+	{
+		return ftp_delete($this->session, $remoteFile);
+	}
+
+
+	public function chmod(string $remoteFile, int $mode = 0644): int|false
+	{
+		return ftp_chmod($this->session, $mode, $remoteFile);
+	}
+
+
+	public function createWebPath(string $path): Url
+	{
+		$url = new Url;
+		$url->setScheme('https');
+		$url->setUser($this->auth->getUsername());
+		$url->setHost($this->host);
+		$url->setPath($path);
+
+		if ($this->auth instanceof Password) {
+			$url->setPassword($this->auth->getPassword());
+		}
+
+		return $url;
+	}
+}
